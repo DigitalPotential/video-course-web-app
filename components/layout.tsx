0777401e96934578f5f78aca1e31c2fc4b899ff2
DigitalPotential <Carlos@digitalpotential.se>
2023-06-18 23:44:11 +0200

    video web app

---
+++ components/layout.tsx
@@ -0,0 +1,13 @@
+
+import Nav from './Nav'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Nav />
+      <main className='mx-auto max-w-full md:max-w-6xl min-h-screen px-5'>
+        {children}
+      </main>
+    </>
+  )
+}
