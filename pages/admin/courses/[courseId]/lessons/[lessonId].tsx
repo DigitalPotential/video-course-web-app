b69ffdee5d7cec109b1ee8b7e097408572a4985b
DigitalPotential <Carlos@digitalpotential.se>
2023-06-24 22:38:47 +0200

    fix: prisma issue

--- pages/admin/courses/[courseId]/lessons/[lessonId].tsx
+++ pages/admin/courses/[courseId]/lessons/[lessonId].tsx
@@ -93,7 +93,7 @@ export default AdminLessonEdit
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getServerSession(context.req, context.res, authOptions)
-
+  console.log(session)
   if (!session) {
     return {
       redirect: {
